dfe26e38fa0a2e15b219239cc618fc0bd02963bb
Asus <s.pishenbaev@newuu.uz>
2023-11-14 18:32:24 +0500

    .

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,21 +1,17 @@
-float problemSolution2(float i, float j, float k) {
-    float result;
-    // write your code here;
-    if(i>=j){
-        if (i>=k){
-            result=i;
-        }
-        else{
-            result = k;
-        }
+float problemSolution1(float consumed_water) {
+    float cost, fixed;
+    fixed = 13;
+    if (consumed_water<=30){
+        cost=fixed+(consumed_water*0.4);
     }
-    else{
-        if (j>=k){
-            result=j;
-        }
-        else{
-            result=k;
-        }
+    else if (consumed_water>30 and consumed_water<=50){
+        cost=fixed+((consumed_water-30)*0.12)+(30*12);
     }
-    return result;
+    else if (consumed_water>50 and consumed_water<=60) {
+        cost=fixed+((consumed_water-50)*1.4)+(30*0.4)+(20*0.12);
+    }
+    else if (consumed_water>60) {
+        cost=fixed+((consumed_water-60)*1.5)+(30*0.4)+(20*0.12)+(10*1.4);
+    }
+    return cost;
 }
