c77a3b742ab6db57a030b0fce712e690afc87a62
Asus <s.pishenbaev@newuu.uz>
2023-11-14 18:25:54 +0500

    .

--- main.cpp
+++ main.cpp
@@ -9,14 +9,35 @@ using namespace std;
 
 int main() {
     cout << "Problem 1\n";
-    problemSolution1();
+    float text1, res1;
+    cin>>text1;
+    res1=problemSolution1(text1);
+    cout<<res1<<endl;
+    cout<<endl;
+
     cout << "Problem 2\n";
-    problemSolution2();
+    float text2p1, text2p2, tex2p3, res2;
+    res2=problemSolution2(text2p1, text2p2, tex2p3);
+    cout<<res2<<endl;
+    cout<<endl;
+
     cout << "Problem 3\n";
-    problemSolution3();
+    float text3p1;
+    char text3p2;
+    basic_string<char> res3;
+    res3=problemSolution3(text3p1,text3p2);
+
     cout << "Problem 4\n";
-    problemSolution4();
+    string text4, res4;
+    cin>>text4;
+    res4 = problemSolution4(text4);
+    cout<<res4<<endl;
+    cout<<endl;
+
     cout << "Problem 5\n";
-    problemSolution5();
+    float text5p1, text5p2, res5;
+    char text5p3;
+    res5=problemSolution5(text5p1, text5p2, text5p3);
+    cout<<res5<<endl;
     return 0;
 }
--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,8 +1,22 @@
 #include <string>
-
 std::string problemSolution3(float height, char S) {
     // write your code here
-
-    // use return to return your result
-    // make use of control flow statements
+    if (height<1.7 and S=='M'){
+        return "Short";
+    }
+    else if (height>=1.70 and height<1.85 and S=='M') {
+        return "Normal";
+    }
+    else if (height>=1.85 and S=='M') {
+        return "Tall";
+    }
+    else if (height<1.6 and S=='F') {
+        return "Short";
+    }
+    else if (height>=1.6 and height<1.75 and S=='F'){
+        return "Normal";
+    }
+    else if (height>=1.75 and S=='F'){
+        return "Tall";
+    }
 }
